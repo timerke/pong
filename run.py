0e439b978c263406e4fa99b317f439ab05672972
timerke <timerke@mail.ru>
2023-04-01 12:39:02 +0300

    Fixed size in computers

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
-from pong.game import PongApp
+from kivy.utils import platform
+from kivy.config import Config
+
+
+def check_platform() -> None:
+    if platform.system().lower() in ("linux", "macosx", "windows"):
+        Config.set("graphics", "resizable", False)
+        Config.set("graphics", "width", "1000")
+        Config.set("graphics", "height", "800")
+        Config.write()
 
 
 if __name__ == "__main__":
+    check_platform()
+
+    from pong.game import PongApp
     PongApp().run()
