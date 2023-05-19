4e22bc70770b3e6339508b55839528c907a65d0c
timerke <timerke@mail.ru>
2023-05-19 18:36:09 +0300

    Added release script for Linux

---
+++ scripts/release.sh
@@ -0,0 +1,16 @@
+cd ..
+rm -rf build
+rm -rf dist
+rm -rf release
+rm -rf venv
+
+python3 -m venv venv
+./venv/bin/python3 -m pip install --upgrade pip
+./venv/bin/python3 -m pip install -r requirements.txt
+./venv/bin/python3 -m pip install pyinstaller
+./venv/bin/python3 -m PyInstaller main.spec
+
+mv dist release
+rm -rf build
+rm -rf dist
+rm -rf venv
