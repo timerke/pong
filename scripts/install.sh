8b9c4107eb67f9e1eb0dcb88cfebf164a2700d9e
timerke <timerke@mail.ru>
2023-05-21 14:29:06 +0300

    Changed scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@ cd ..
 rm -rf venv
 python3 -m venv venv
 ./venv/bin/python3 -m pip install --upgrade pip
-./venv/bin/python3 -m pip install -r requirements.txt --no-binary kivy kivy
+./venv/bin/python3 -m pip install -r requirements.txt
--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,6 @@ rm -rf venv
 python3 -m venv venv
 ./venv/bin/python3 -m pip install --upgrade pip
 ./venv/bin/python3 -m pip install -r requirements.txt
-./venv/bin/python3 -m pip uninstall kivy
-./venv/bin/python3 -m pip install --no-binary kivy kivy
 ./venv/bin/python3 -m pip install pyinstaller
 ./venv/bin/python3 -m PyInstaller main.spec
 
