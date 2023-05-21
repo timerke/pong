36f90a37e5bd82b57b3a6e3d3d13a17b4460d6ab
timerke <timerke@mail.ru>
2023-05-21 15:39:36 +0300

    Changes linux release

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,11 @@ python3 -m venv venv
 ./venv/bin/python3 -m pip install --upgrade pip
 ./venv/bin/python3 -m pip install -r requirements.txt
 ./venv/bin/python3 -m pip install pyinstaller
-./venv/bin/python3 -m PyInstaller main.spec
+./venv/bin/pyinstaller main.py --clean --onefile --noconsole \
+--add-data "./media/*:media" \
+--icon media/icon.ico \
+--splash media/icon.png \
+--name pong
 
 mv dist release
 rm -rf build
